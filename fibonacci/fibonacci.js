e0094ebd5d0e28bf9907638b63f64ace4971a140
tiagoheras <tiagoheras@hotmail.com>
2021-02-24 09:54:35 -0300

    complete fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function(num) {
-    const fibonacciArray = [1];
-    fibonacciArray.
+const fibonacci = function (num) {
+    const fibonacciArray = [1, 1];
+    for (var i = 0; i < num; i++) {
+        let result = fibonacciArray[fibonacciArray.length - 1] + fibonacciArray[fibonacciArray.length - 2];
+        fibonacciArray.push(result);
+    }
+    return fibonacciArray[num - 1];
 }
+
+console.log(fibonacci(4));
 
 module.exports = fibonacci
